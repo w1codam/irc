4f945ee6e696fa4d3b032563d4f1185e1bf779c1
Justus de Groot <justus@macbook.local>
2023-06-13 20:09:36 +0200

    channel class (header) as good as finished

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -11,18 +11,42 @@ class Channel
 {
 private:
 	std::string				_name;
+	std::string				_topic;
+	std::string				_password;
+
 	std::vector<Client*>	_members;
 	std::vector<Client*>	_operators;
+	std::vector<Client*>	_invited;	// able to join if channel is invite only
+
+	bool					_invite_only;
+	bool					_topic_ops_only;
+	uint32_t				_user_limit;
+public:
+	void		setTopic(std::string topic);
+	void		setPassword(std::string password);
+	void		setInviteOnly(bool mode);
+	void		setTopicOpsOnly(bool mode);
+	void		setUserLimit(uint32_t limit);
+
+	std::string	getTopic();
+	bool		getInviteOnly();
+	bool		getTopicOpsOnly();
+	uint32_t	getUserLimit();
+	bool		checkPassword(std::string password);
 public:
 	void	sendMessage(std::string message);	// for each member, append this to queue
 
 	bool	isMember(Client* client);
 	bool	isOperator(Client* client);
+	bool	isInvited(Client* client);
 
 	void	addUser(Client* client);
-	void	removeUser(Client* client);
 	void	addOperator(Client* client);
+	void	addInvited(Client* client);
+
+	void	removeUser(Client* client);
 	void	removeOperator(Client* client);
+	void	removeInvited(Client* client);
 };
 
 #endif
