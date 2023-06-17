343fc755d5cda3f1f30fbdd0ae9e67132869db2c
Justus de Groot <justus@macbook.local>
2023-06-17 14:30:41 +0200

    added function which handles all incoming packets until EAGAIN for clients

--- src/Client/Receive.cpp
+++ src/Client/Receive.cpp
@@ -26,6 +26,35 @@ bool			Client::receivePacket()
 	return true;
 }
 
+// // experimental
+// // receive until errno == EAGAIN and return this->_buffer.find("\r\n") != std::string::npos
+// bool			Client::receivePacket()
+// {
+// 	ssize_t	received;
+// 	char	buffer[1024];	// pretty sure max length for a single packet is 512 bytes but lets see (513 to set the null-byte)
+
+// 	while (true)
+// 	{
+// 		received = Networking::Recv(this->_socket, buffer, sizeof(buffer) - 1);
+
+// 		if (received == -1 && errno == EAGAIN)
+// 		{
+// 			errno = 0;
+// 			return this->_buffer.find("\r\n") != std::string::npos;
+// 		}
+// 		if (received == -1)
+// 			throw Networking::NetworkingException("receivePacket() -> Recv() failure and errno != EAGAIN");
+// 		if (received == 0) // peer disconnect, no idea what we do here, for now throw an exception
+// 			throw Networking::NetworkingException("receivePacket() peer disconnected");
+
+// 		buffer[received] = '\0';
+// 		this->_buffer += buffer;
+// 	}
+
+// 	return true;
+// }
+
+
 std::string		Client::getPacket()
 {
 	size_t		pos;
